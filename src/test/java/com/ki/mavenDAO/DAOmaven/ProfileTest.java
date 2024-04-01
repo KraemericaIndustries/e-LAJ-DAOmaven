2b8be75c1c3d11f3b8b9447ee8d52151f6e34ee9
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-01 08:32:17 -0400

    Verified code working through lecture 75: "Testing the Profile Class"

---
+++ src/test/java/com/ki/mavenDAO/DAOmaven/ProfileTest.java
@@ -0,0 +1,22 @@
+package com.ki.mavenDAO.DAOmaven;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ProfileTest {
+
+	@Test
+	public void testLoadDbConfig() {
+		
+		var props = Profile.getProperties("db");
+		
+		assertNotNull("Cannot load db properties", props);
+		
+		var dbName = props.getProperty("database");
+		
+		assertEquals("dbName incorrect", "peopletest", dbName);
+	}
+}
