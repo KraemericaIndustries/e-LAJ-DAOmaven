1fac0cfe349119fef2336bda5d0a4b9e8955c81d
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-01 08:43:57 -0400

    Verified code working through lecture 76: "Creating MySQL Users"

--- src/test/java/com/ki/mavenDAO/DAOmaven/ProfileTest.java
+++ src/test/java/com/ki/mavenDAO/DAOmaven/ProfileTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 public class ProfileTest {
 
 	@Test
