1d089353377df0eb0e73161b492c7a27cd1665dd
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-01 09:27:06 -0400

    Verified code working through lecture 79: "Loading a Text File"

--- src/test/java/com/ki/mavenDAO/DAOmaven/UserDaoImplTest.java
+++ src/test/java/com/ki/mavenDAO/DAOmaven/UserDaoImplTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,9 +18,24 @@ import org.junit.Before;
 public class UserDaoImplTest {
 	
 	private Connection conn;
+	private List<User> users;
+	
+	private static final int NUM_TEST_USERS = 1000;
+	
+	private List<User> loadUsers() throws IOException {
+		
+		var temp = Files
+			.lines(Paths.get("../FiveLetterWords.txt"));
+		temp.forEach(System.out::println);
+		
+		return null;
+	}
 	
 	@Before
-	public void setUp() throws SQLException {
+	public void setUp() throws SQLException, IOException {
+		
+		users = loadUsers();
+		
 		var props = Profile.getProperties("db");
 		
 		var db = Database.instance();
